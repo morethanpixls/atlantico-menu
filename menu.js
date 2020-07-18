2fbde7264c5df07b3bafde65589f5dd3397cf74f
Orlando Arias <hello@orlandomario.com>
2020-07-18 18:27:53 -0400

    Updated atlanticoMenu function to become an iffe that takes three arguments. Also updated .gitignore to exclude .vscode

--- menu.js
+++ menu.js
@@ -1,11 +1,24 @@
-// Selectors
-const toggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".menu");
+// ATLANTICO MENU
+// Documentation
+// "containerSelector" taks any class or id for the parent div
+// "toggleSelector" takes any class or id for the child div
+// "position" should have any of the following values: top, right, bottom, left
 
-// Toggle Menu
-function showMenu() {
-    menu.classList.toggle("is-active");
-}
+(function atlanticoMenu(
+	containerSelector = ".menu",
+	toggleSelector = ".menu-toggle",
+	position = "left",
+) {
+	const toggle = document.querySelector(toggleSelector);
+	const menu = document.querySelector(containerSelector);
 
-// Click
-toggle.addEventListener("click", showMenu);
+	// Toggle Menu
+	function showMenu() {
+		menu.classList.toggle("is-active");
+	}
+
+	// Click
+	toggle.addEventListener("click", showMenu);
+
+	console.log(position);
+})();
